c377639c7af1f8ba8dff7d1c923e54058d6fdd3b
megzimo <40956115+megzimo@users.noreply.github.com>
2018-12-18 11:31:47 -0600

    basic components created

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+function Header(props) {
+    return(
+        <div>Here is where the score will be kept
+        </div>
+    )
+}
+
+export default Header;
